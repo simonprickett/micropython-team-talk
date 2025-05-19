8589ee4ea89a7672e8af0637a1972cb814ca17f0
Simon Prickett <simon.prickett@grafana.com>
2025-05-19 15:21:03 +0100

    Minor updates for hello world.

--- hello-world/blink.py
+++ hello-world/blink.py
@@ -4,11 +4,13 @@ from utime import sleep
 pin = Pin("LED", Pin.OUT)
 
 print("LED starts flashing...")
+
 while True:
     try:
         pin.toggle()
-        sleep(1) # sleep 1sec
+        sleep(1)
     except KeyboardInterrupt:
         break
+
 pin.off()
 print("Finished.")
